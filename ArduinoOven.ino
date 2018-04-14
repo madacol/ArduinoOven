a98e853f55f763239d11116b762cab0cdc9db1ec
madacol <madacol10@gmail.com>
2018-04-14 15:43:07 -0500

    Fix: Touching orientation didn't match with display

--- ArduinoOven.ino
+++ ArduinoOven.ino
@@ -411,8 +411,8 @@ TSPoint getAvgTouchPoint(void)
   }
   if (count < QUORUM) tp.z=0;
   else { tp.x /= count; tp.y /= count; tp.z /= count; } // get average
-  int temp = map(tp.y, 85, 896, dispX-1, 0);
-  tp.y = map(tp.x, 907, 130, dispY-1, 0);
+  int temp = map(tp.y, 85, 896, 0, dispX-1);
+  tp.y = map(tp.x, 907, 130, 0, dispY-1);
   tp.x = temp;
   return tp;
 }
